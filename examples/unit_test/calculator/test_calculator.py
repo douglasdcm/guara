2f097c5c2d8841cc4d990060baf4be5bd3bfbb0d
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-01-15 15:03:18 -0300

    Improvements in documentation

--- examples/unit_test/calculator/test_calculator.py
+++ examples/unit_test/calculator/test_calculator.py
@@ -9,20 +9,17 @@ class TestCalculator:
     def setup_method(self, method):
         self._calculator = Application(Calculator())
 
-    def test_add_return_3_when_add_1_to_2(self):
+    def test_add_returns_3_when_adding_1_and_2(self):
         text = ["cheese", "selenium", "test", "bla", "foo"]
         text = text[random.randrange(len(text))]
-
         self._calculator.at(operations.Add, a=1, b=2).asserts(it.IsEqualTo, 3)
 
-    def test_add_return_1_when_add_1_to_0(self):
+    def test_add_returns_1_when_adding_1_to_0(self):
         text = ["cheese", "selenium", "test", "bla", "foo"]
         text = text[random.randrange(len(text))]
-
         self._calculator.at(operations.Add, a=1, b=0).asserts(it.IsEqualTo, 1)
 
-    def test_add_return_2_when_subtract_1_from_2(self):
+    def test_add_returns_2_when_subtracting_1_from_2(self):
         text = ["cheese", "selenium", "test", "bla", "foo"]
         text = text[random.randrange(len(text))]
-
         self._calculator.at(operations.Subtract, a=2, b=1).asserts(it.IsEqualTo, 1)
