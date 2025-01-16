73bc050aaee61250bfb703583b143f1936009d84
theijhay <olawaleisaacjohn@gmail.com>
2025-01-17 00:33:31 +0100

    Add Appium integration for Selenium web UI tests

--- examples/web_ui/selenium/appium/setup.py
+++ examples/web_ui/selenium/appium/setup.py
@@ -20,6 +20,8 @@ class OpenAppiumApp(AbstractTransaction):
             'platformName': 'Android',
             'deviceName': 'emulator-5554',
             'browserName': 'Chrome',
+            "app": "/path/to/sample.apk",
+            "automationName": "UiAutomator2",
             'chromeOptions': {'args': ['--headless']} if headless else {}
         }
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
