5ee62abbc5de6c1ab6b435d45b8ff57c7dba046a
DONALDBZR <andygaspard@hotmail.com>
2025-01-16 19:48:03 +0400

    [QA][V0.0.7][0.0.20] The module that has the interface for the implmentation of the assertion logic to be used for validation and testing.

---
+++ guara/asynchronous/assertion.py
@@ -0,0 +1,49 @@
+"""
+The module that has the interface for the implmentation of
+the assertion logic to be used for validation and testing.
+
+Authors:
+    douglasdcm
+    Darkness4869
+"""
+from typing import Any, Coroutine
+from logging import getLogger, Logger
+
+
+LOGGER: Logger = getLogger(__name__)
+class IAssertion:
+    """
+    It is the base class for implementing assertion logic which
+    is used for validation and testing.
+    """
+    async def asserts(self, actual: Any, expected: Any) -> Coroutine[None, None, None]:
+        """
+        It defines the assertion logic by comparing the actual data
+        against the expected data.
+
+        Parameters:
+            actual: Any: The actual data
+            expected: Any: The expected data
+
+        Returns:
+            void
+        """
+        raise NotImplementedError
+
+    async def validates(self, actual: Any, expected: Any) -> Coroutine[None, None, None]:
+        """
+        Executing the assertion logic.
+
+        Parameters:
+            actual: Any: The actual data
+            expected: Any: The expected data
+
+        Returns:
+            void
+        """
+        try:
+            await self.asserts(actual, expected)
+        except Exception:
+            LOGGER.error(f"actual: {actual.result}")
+            LOGGER.error(f"expected: {expected}")
+            raise
