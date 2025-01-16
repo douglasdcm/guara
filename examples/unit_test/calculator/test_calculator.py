a60c12205cd83b5d5902086de3c7e8d3b64a93a6
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-01-16 00:41:44 -0300

    Add examples of execution with other test frameworks - Example with unittest - Example with testify - Example with stestr - Example with green

--- examples/unit_test/calculator/test_calculator.py
+++ examples/unit_test/calculator/test_calculator.py
@@ -1,4 +1,6 @@
 import random
+import unittest
+import testify
 from examples.unit_test.calculator.calculator import Calculator
 from examples.unit_test.calculator import operations
 from guara.transaction import Application
@@ -23,3 +25,24 @@ class TestCalculator:
         text = ["cheese", "selenium", "test", "bla", "foo"]
         text = text[random.randrange(len(text))]
         self._calculator.at(operations.Subtract, a=2, b=1).asserts(it.IsEqualTo, 1)
+
+
+class TestCalculatorTestCase(unittest.TestCase):
+    def setUp(self):
+        self._calculator = Application(Calculator())
+
+    def test_add_returns_3_when_adding_1_and_2(self):
+        text = ["cheese", "selenium", "test", "bla", "foo"]
+        text = text[random.randrange(len(text))]
+        self._calculator.at(operations.Add, a=1, b=2).asserts(it.IsEqualTo, 3)
+
+
+class TestCalculatorTestify(testify.TestCase):
+    @testify.setup
+    def setup_method(self):
+        self._calculator = Application(Calculator())
+
+    def test_add_returns_3_when_adding_1_and_2(self):
+        text = ["cheese", "selenium", "test", "bla", "foo"]
+        text = text[random.randrange(len(text))]
+        self._calculator.at(operations.Add, a=1, b=2).asserts(it.IsEqualTo, 3)
--- guara/__init__.py
+++ guara/__init__.py
@@ -5,7 +5,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s.%(msecs)03d %(levelname)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    force=True 
+    force=True,
 )
 
 LOGGER = logging.getLogger("guara")
