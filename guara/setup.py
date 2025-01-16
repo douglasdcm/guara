8d6d8202837ce3e291d1d4d66a61af8533970779
DONALDBZR <andygaspard@hotmail.com>
2025-01-16 18:31:19 +0400

    [QA][V0.0.7][0.0.13] The module that is reponsible for the opening and closing transactions.

--- guara/setup.py
+++ guara/setup.py
@@ -1,25 +1,37 @@
+"""
+The module that is reponsible for the opening and closing
+transactions.
+"""
 from datetime import datetime
-from guara.transaction import AbstractTransaction
+from guara.transaction import AbstractTransaction, WebDriver
 
 
 class OpenApp(AbstractTransaction):
     """
-    Opens the app
-
-    Args:
-        url (str): the path where the screenshot is saved.
-        window_width (int): The width of the browser. Defaults to 1094
-        window_height (int): The height of the browser. Defaults t0 765
-        implicitly_wait (int): the implicity timeout for an element to be found.
-        Defaults to 10 (seconds)
-    Returns:
-        str: the title of the app
+    It is the transaction for opening application.
     """
+    def __init__(self, driver: WebDriver):
+        """
+        Initializing the transaction
 
-    def __init__(self, driver):
+        Parameters:
+            driver: WebDriver: The web driver
+        """
         super().__init__(driver)
 
-    def do(self, url, window_width=1094, window_height=765, implicitly_wait=10):
+    def do(self, url: str, window_width: int = 1094, window_height: int = 765, implicitly_wait: int = 10) -> str:
+        """
+        It performs a specific transaction
+
+        Parameters:
+            url: string: The path of the directory of the screenshots.
+            window_width: int: The width of the browser
+            window_height: int: The height of the browser
+            implicitly_wait: int: The implicity timeout for an element to be found
+
+        Returns:
+            string
+        """
         self._driver.set_window_size(window_width, window_height)
         self._driver.get(url)
         self._driver.implicitly_wait(implicitly_wait)
@@ -28,20 +40,26 @@ class OpenApp(AbstractTransaction):
 
 class CloseApp(AbstractTransaction):
     """
-    Closes the app and saves its screenshot (PNG)
-
-    Args:
-        screenshot_filename (str): the path where the screenshot is saved.
-        Examples: './myfile', '/path/to/myfile'
-        Defaults to 'guara-{datetime.now()}.png'.
-
+    It is the transaction for closing application.
     """
+    def __init__(self, driver: WebDriver):
+        """
+        Initializing the transaction
 
-    def __init__(self, driver):
+        Parameters:
+            driver: WebDriver: The web driver
+        """
         super().__init__(driver)
 
-    def do(self, screenshot_filename="./captures/guara-capture"):
-        self._driver.get_screenshot_as_file(
-            f"{screenshot_filename}-{datetime.now()}.png"
-        )
+    def do(self, screenshot_filename="./captures/guara-capture") -> None:
+        """
+        It performs a specific transaction
+
+        Parameters:
+            screenshot_filename: string: The path of the directory of the screenshots.
+
+        Returns:
+            void
+        """
+        self._driver.get_screenshot_as_file(f"{screenshot_filename}-{datetime.now()}.png")
         self._driver.quit()
