e4a91f39869f9f7a7abbaa9b5f6d6ab6aedfc281
DONALDBZR <andygaspard@hotmail.com>
2025-01-16 18:34:52 +0400

    [QA][V0.0.7][0.0.15] The module to be used to retrieve the information of the transaction.

--- guara/utils.py
+++ guara/utils.py
@@ -1,6 +1,23 @@
 """
+The module to be used to retrieve the information of the
+transaction.
 
+Authors:
+    douglasdcm
+    RonaldTheodoro
 """
-def get_transaction_info(transaction) -> str:
-    module_name = ".".join(transaction.__module__.split(".")[-1:])
+from typing import Any
+
+
+def get_transaction_info(transaction: Any) -> str:
+    """
+    Retrieving the information of a transaction.
+
+    Parameters:
+        transaction: Any: The transaction object.
+
+    Returns:
+        string
+    """
+    module_name: str = ".".join(transaction.__module__.split(".")[-1:])
     return f"{module_name}.{transaction.__name__}"
