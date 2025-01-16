319d93de93144c9709441afd477d42c76757a66e
DONALDBZR <andygaspard@hotmail.com>
2025-01-16 23:40:08 +0400

    [QA][V0.0.7][0.0.28] The module that is reponsible for the opening and closing transactions.

--- guara/asynchronous/setup.py
+++ guara/asynchronous/setup.py
@@ -1,37 +1,66 @@
-from guara.asynchronous.transaction import AbstractTransaction
+"""
+The module that is reponsible for the opening and closing
+transactions.
+
+Authors:
+    douglasdcm
+    RonaldTheodoro
+"""
+from guara.asynchronous.transaction import AbstractTransaction, WebDriver
+from typing import Dict, Any
 
 
 class OpenApp(AbstractTransaction):
     """
+    It is the transaction for opening application.
+
     Not Implemented as Selenium is not executed asynchronously.
     Use your preferable asynchronous Web Driver.
-    For example: https://github.com/douglasdcm/caqui
+    
+    Link:
+        https://github.com/douglasdcm/caqui
     """
 
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
+        """
+        Initializing the transaction
+
+        Parameters:
+            driver: WebDriver: The web driver
+        """
         super().__init__(driver)
 
-    async def do(self, **kwargs):
-        raise NotImplementedError(
-            "Selenium does not support asynchronous execution."
-            " Use your preferable async WebDriver. "
-            " For example https://github.com/douglasdcm/caqui"
-        )
-
+    async def do(self, **kwargs: Dict[str, Any]) -> Any:
+        raise NotImplementedError("Selenium does not support asynchronous execution.\nUse your preferable async WebDriver.\nFor example https://github.com/douglasdcm/caqui")
 
 class CloseApp(AbstractTransaction):
     """
+    It is the transaction for closing application.
+
     Not Implemented as Selenium is not executed asynchronously.
     Use your preferable asynchronous Web Driver.
-    For example: https://github.com/douglasdcm/caqui
+    
+    Link:
+        https://github.com/douglasdcm/caqui
     """
 
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
+        """
+        Initializing the transaction
+
+        Parameters:
+            driver: WebDriver: The web driver
+        """
         super().__init__(driver)
 
-    async def do(self, **kwargs):
-        raise NotImplementedError(
-            "Selenium does not support asynchronous execution."
-            " Use your preferable async WebDriver. "
-            " For example https://github.com/douglasdcm/caqui"
-        )
+    async def do(self, **kwargs: Dict[str, Any]) -> Any:
+        """
+        It performs a specific transaction
+
+        Parameters:
+            kwargs: dict: It contains all the necessary data and parameters for the transaction.
+
+        Returns:
+            Any
+        """
+        raise NotImplementedError("Selenium does not support asynchronous execution.\nUse your preferable async WebDriver.\nFor example https://github.com/douglasdcm/caqui")
