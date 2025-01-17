dc74c3f80802cd5a82525e066fdd5aa54510763b
theijhay <olawaleisaacjohn@gmail.com>
2025-01-17 03:18:54 +0100

    Refactor Appium setup to use absolute path for app and add additional capabilities

--- examples/mobile/appium/test_appium_integration.py
+++ examples/mobile/appium/test_appium_integration.py
@@ -1,29 +1,24 @@
 import pathlib
 import random
 from appium import webdriver
-from appium.options.android import UiAutomator2Options
 from guara.transaction import Application
 from guara import it
 from setup import OpenAppiumApp, CloseAppiumApp
 from home import SubmitTextAppium
-
 class TestAppiumIntegration:
     def setup_method(self, method):
         file_path = pathlib.Path(__file__).parent.resolve()
-
-        """UiAutomator2Options for modern capabilities setup"""
-        options = UiAutomator2Options()
-        options.platform_name = "Android"
-        options.device_name = "emulator-5554"
-        options.browser_name = "Chrome"
-        options.app = "/absolute/path/to/sample.apk"
-        options.automation_name = "UiAutomator2"
-        options.no_reset = True
-        options.app_wait_activity = "*"
-        options.chrome_options = {'args': ['--headless']}
-
-        """Options object instead of desired_capabilities"""
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', options=options)
+        desired_caps = {
+            'platformName': 'Android',
+            'deviceName': 'emulator-5554',
+            'browserName': 'Chrome',
+            "app": "/absolute/path/to/sample.apk",
+            "automationName": "UiAutomator2",
+            "noReset": True,
+            "appWaitActivity": "*",
+            'goog:chromeOptions': {'args': ['--headless']}
+        }
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities=desired_caps)
         self._app = Application(self.driver)
         self._app.at(OpenAppiumApp, url=f"file:///{file_path}/sample.html")
 
