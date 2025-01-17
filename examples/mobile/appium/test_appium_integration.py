074ef7390d90609086ee0a62572c07b5fee729f0
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-01-17 19:05:05 -0300

    Fix linter issues and add new libraries to test-requirements.txt

--- examples/mobile/appium/test_appium_integration.py
+++ examples/mobile/appium/test_appium_integration.py
@@ -7,21 +7,24 @@ from guara import it
 from setup import OpenAppiumApp, CloseAppiumApp
 from home import SubmitTextAppium
 
+
 @pytest.mark.skip(reason="Complex setup in CI environment")
 class TestAppiumIntegration:
     def setup_method(self, method):
         file_path = pathlib.Path(__file__).parent.resolve()
         desired_caps = {
-            'platformName': 'Android',
-            'deviceName': 'emulator-5554',
-            'browserName': 'Chrome',
+            "platformName": "Android",
+            "deviceName": "emulator-5554",
+            "browserName": "Chrome",
             "app": "/absolute/path/to/sample.apk",
             "automationName": "UiAutomator2",
             "noReset": True,
             "appWaitActivity": "*",
-            'goog:chromeOptions': {'args': ['--headless']}
+            "goog:chromeOptions": {"args": ["--headless"]},
         }
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities=desired_caps)
+        self.driver = webdriver.Remote(
+            "http://localhost:4723/wd/hub", desired_capabilities=desired_caps
+        )
         self._app = Application(self.driver)
         self._app.at(OpenAppiumApp, url=f"file:///{file_path}/sample.html")
 
