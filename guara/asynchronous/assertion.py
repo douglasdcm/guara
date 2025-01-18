156286f5f7757732eb3b3881cd6708350fc70bcd
DONALDBZR <andygaspard@hotmail.com>
2025-01-18 22:22:11 +0400

    [QA][V0.0.7][0.0.37] Resolving the issue of the adssertion interface where the correct documentation for python.

--- guara/asynchronous/assertion.py
+++ guara/asynchronous/assertion.py
@@ -1,12 +1,8 @@
 """
 The module that has the interface for the implmentation of
 the assertion logic to be used for validation and testing.
-
-Authors:
-    douglasdcm
-    Darkness4869
 """
-from typing import Any, Coroutine
+from typing import Any
 from logging import getLogger, Logger
 
 
@@ -21,12 +17,12 @@ class IAssertion:
         It defines the assertion logic by comparing the actual data
         against the expected data.
 
-        Parameters:
-            actual: Any: The actual data
-            expected: Any: The expected data
+        Args:
+            actual: (Any): The actual data
+            expected: (Any): The expected data
 
         Returns:
-            void
+            (None)
         """
         raise NotImplementedError
 
