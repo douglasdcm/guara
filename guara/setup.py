d531191d175b674119059b3498c4028b3cfcb84a
DONALDBZR <andygaspard@hotmail.com>
2025-01-18 23:15:50 +0400

    [QA][V0.0.7][0.0.80] It performs a specific transaction

--- guara/setup.py
+++ guara/setup.py
@@ -20,18 +20,6 @@ class OpenApp(AbstractTransaction):
         super().__init__(driver)
 
     def do(self, url: str, window_width: int = 1094, window_height: int = 765, implicitly_wait: int = 10) -> str:
-        """
-        It performs a specific transaction
-
-        Parameters:
-            url: string: The path of the directory of the screenshots.
-            window_width: int: The width of the browser
-            window_height: int: The height of the browser
-            implicitly_wait: int: The implicity timeout for an element to be found
-
-        Returns:
-            string
-        """
         self._driver.set_window_size(window_width, window_height)
         self._driver.get(url)
         self._driver.implicitly_wait(implicitly_wait)
