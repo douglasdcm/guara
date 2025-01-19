d15415a2d7398d8ac408961c2f8877abda726b96
theijhay <olawaleisaacjohn@gmail.com>
2025-01-19 11:32:03 +0100

    Refactor import statements and update variable names in home.py

--- examples/web_ui/splinter/home.py
+++ examples/web_ui/splinter/home.py
@@ -13,14 +13,13 @@ class SubmitTextSplinter(AbstractTransaction):
         str: the label 'It works! {code}!'
     """
 
-    def __init__(self, driver):
-        super().__init__(driver)
-        self._driver: Browser
-
     def do(self, text):
         TEXT = '//*[@id="input"]'
-        BUTTON_TEST = '//*[@id="button"]'
+        BUTTON_TEST = "button"
         RESULT = '//*[@id="result"]'
-        self._driver.input.value(TEXT, text)
-        self._driver.click.button(BUTTON_TEST)
-        return self._driver.get.text(RESULT)
+        text_field = self.browser.find_by_tag(TEXT).first
+        text_field.fill(text)
+        button = self.browser.find_by_tag(BUTTON_TEST).first
+        button.click()
+        result = self.browser.find_by_id(RESULT).first
+        return result.text
