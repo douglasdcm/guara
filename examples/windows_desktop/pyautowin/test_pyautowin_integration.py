fe55bc697bfb411b53600e32952a918dbfa5a8f1
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-01-23 01:13:17 -0300

    Organize folders

--- examples/windows_desktop/pyautowin/test_pyautowin_integration.py
+++ examples/windows_desktop/pyautowin/test_pyautowin_integration.py
@@ -12,20 +12,20 @@ class TestPyAutoWinIntegration:
     def setup_method(self, method):
         """Lazy import to avoid triggering module imports"""
         import pyautowin
-        from examples.linux_desktop.pyautowin import setup
+        from examples.windows_desktop.pyautowin import setup
 
         self._app = Application(pyautowin)
         self._app.at(setup.OpenApplication, app_path="path/to/application.exe")
 
     def teardown_method(self, method):
         """Lazy import to avoid triggering module imports"""
-        from examples.linux_desktop.pyautowin import setup
+        from examples.windows_desktop.pyautowin import setup
 
         self._app.at(setup.CloseApplication, app_name="Application Name")
 
     def test_submit_text(self):
         """Lazy import to avoid triggering module imports"""
-        from examples.linux_desktop.pyautowin import home
+        from examples.windows_desktop.pyautowin import home
 
         text = "Hello, PyAutoWin!"
         self._app.at(home.SubmitTextPyAutoWin, text=text).asserts(it.IsEqualTo, text)
