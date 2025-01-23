87160bd4894fc5aca62265fad6e39d28b46ec0c8
Darkness4869 <33237857+DONALDBZR@users.noreply.github.com>
2025-01-23 22:38:06 +0400

    QA and Review V0.0.8 (#69)
    * [QA][V0.0.8][0.0.1]
    The module that has the interface for the implmentation of the assertion logic to be used for validation and testing.
    
    * [QA][V0.0.8][0.0.2]
    The transaction for opening the application.
    
    * [QA][V0.0.8][0.0.3]
    The transaction for closing the application.
    
    * [QA][V0.0.8][0.0.4]
    Performing a transaction.
    
    * [QA][V0.0.8][0.0.5]
    Executing each transaction.
    
    * [QA][V0.0.8][0.0.6]
    Retrieving the transaction from the coroutine.
    
    * [QA][V0.0.8][0.0.7]
    Retrieving the assertion from the coroutine.
    
    * [QA][V0.0.8][0.0.8]
    Closes the app and saves its screenshot (PNG)
    
    * [QA][V0.0.8][0.0.9]
    Opens the app
    
    * [QA][V0.0.8][0.0.10]
    Executing all of the coroutines.
    
    * [QA][V0.0.8][0.0.11]
    Retrieving the assertion from the coroutine.

--- guara/transaction.py
+++ guara/transaction.py
@@ -49,7 +49,9 @@ class Application:
         return self._result
 
     def at(
-        self, transaction: AbstractTransaction, **kwargs: Dict[str, Any]
+        self,
+        transaction: AbstractTransaction,
+        **kwargs: Dict[str, Any]
     ) -> "Application":
         """
         Performing a transaction.
