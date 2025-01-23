1bc5d4a398b22ee15e8dede88dd51dac899d9cb0
theijhay <olawaleisaacjohn@gmail.com>
2025-01-23 16:45:14 +0100

    Refactor Selene integration setup and imports in test_selene_integration.py

--- examples/web_ui/selene/test_selene_integration.py
+++ examples/web_ui/selene/test_selene_integration.py
@@ -1,10 +1,9 @@
-import pytest
 from pathlib import Path
+from random import randrange
 from guara.transaction import Application
 from guara import it
 
 
-@pytest.mark.skip(reason="Complex setup in CI environment")
 class TestSeleneIntegration:
     """
     TestSeleneIntegration is a test class for integrating Selene with a local web page.
@@ -15,10 +14,9 @@ class TestSeleneIntegration:
         from selene import browser
         from examples.web_ui.selene import setup
 
-        file_path = Path(__file__).parent.parent.parent.resolve()
+        file_path = Path(__file__).parent.parent.resolve()
         self._app = Application(browser)
-        self._app.at(setup.OpenSeleneApp,
-                     url=f"file:///{file_path}/sample.html")
+        self._app.at(setup.OpenSeleneApp, url=f"file:///{file_path}/sample.html")
 
     def teardown_method(self, method):
         """Lazy import to avoid triggering module imports"""
@@ -26,9 +24,13 @@ class TestSeleneIntegration:
 
         self._app.at(setup.CloseSeleneApp)
 
-    def test_submit_text(self):
+    def test_local_page(self):
         """Lazy import to avoid triggering module imports"""
         from examples.web_ui.selene import home
 
-        text = "Hello, Selene!"
-        self._app.at(home.SubmitTextSelene, text=text).asserts(it.IsEqualTo, text)
+        text = ["cheese", "selenium", "test", "bla", "foo"]
+        text = text[randrange(len(text))]
+        self._app.at(home.SubmitTextSelene, text=text).asserts(
+            it.IsEqualTo, f"It works! {text}!"
+        )
+        self._app.at(home.SubmitTextSelene, text=text).asserts(it.IsNotEqualTo, "Any")
