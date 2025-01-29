e213c36d5fb76766a10eb825a009fd464f20b061
theijhay <olawaleisaacjohn@gmail.com>
2025-01-29 03:27:59 +0100

    Refactor OpenStealthBrowser initialization in test_selenium_stealth.py

--- examples/web_ui/selenium_stealth/test_selenium_stealth.py
+++ examples/web_ui/selenium_stealth/test_selenium_stealth.py
@@ -14,7 +14,7 @@ class TestSeleniumStealthIntegration:
     def setup_method(self, method):
         self._app = Application(None)
         driver = self._app.at(OpenStealthBrowser, headless=True)
-        self._app = Application(driver)
+        self._app.self._driver(driver)
 
     def teardown_method(self, method):
         self._app.at(CloseStealthBrowser)
