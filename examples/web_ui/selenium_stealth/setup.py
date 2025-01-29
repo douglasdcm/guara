f3f359427a4acfb07f161ad1dd42d5f5a63b13cf
theijhay <olawaleisaacjohn@gmail.com>
2025-01-29 02:12:57 +0100

    Add Selenium Stealth integration tests

---
+++ examples/web_ui/selenium_stealth/setup.py
@@ -0,0 +1,35 @@
+from guara.transaction import AbstractTransaction
+from selenium import webdriver
+from selenium_stealth import stealth
+
+
+class OpenStealthBrowser(AbstractTransaction):
+    """Initialize stealth browser"""
+    
+    def __init__(self, driver=None):
+        super().__init__(driver)
+
+    def do(self, headless: bool = True):
+        options = webdriver.ChromeOptions()
+        if headless:
+            options.add_argument("--headless")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        
+        self._driver = webdriver.Chrome(options=options)
+        stealth(
+            self.driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            fix_hairline=True,
+        )
+        return self._driver
+
+class CloseStealthBrowser(AbstractTransaction):
+    """Close stealth browser"""
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def do(self):
+        if self._driver:
+            self._driver.quit()
---
+++ examples/web_ui/selenium_stealth/test_selenium_stealth.py
@@ -0,0 +1,27 @@
+from guara.transaction import Application
+from guara import it
+from examples.web_ui.selenium_stealth.setup import OpenStealthBrowser, CloseStealthBrowser
+from examples.web_ui.selenium_stealth.home import HomeTransactions
+from random import randrange
+
+
+class TestSeleniumStealthIntegration:
+    """
+    TestSeleniumStealthIntegration is a test class 
+    for integrating Selenium Stealth with a local web page.
+    """
+
+    def setup_method(self, method):
+        self._app = Application(None)
+        self._app.at(OpenStealthBrowser, headless=True)
+
+    def teardown_method(self, method):
+        self._app.at(CloseStealthBrowser)
+
+    def test_local_page(self):
+        text = ["cheese", "selenium", "test", "bla", "foo"]
+        text = text[randrange(len(text))]
+        self._app.at(HomeTransactions, text=text).asserts(
+            it.IsEqualTo, f"It works! {text}!"
+        )
+        self._app.at(HomeTransactions, text=text).asserts(it.IsNotEqualTo, "Any")
