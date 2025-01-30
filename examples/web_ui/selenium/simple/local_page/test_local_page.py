09c2a23ae7c389289983b84c5a97e6f8f0cd0b42
DONALDBZR <andygaspard@hotmail.com>
2025-01-31 00:21:12 +0400

    [QA][0.0.9][0.0.24] The testing module that allows the testing of Selenium on local pages.

--- examples/web_ui/selenium/simple/local_page/test_local_page.py
+++ examples/web_ui/selenium/simple/local_page/test_local_page.py
@@ -1,33 +1,47 @@
+"""
+The testing module that allows the testing of Selenium on
+local pages.
+"""
 from pathlib import Path
 from random import randrange
-from selenium import webdriver
 from examples.web_ui.selenium.simple.local_page import home
 from guara.transaction import Application
-from guara import it
-from guara import setup
+from selenium.webdriver import ChromeOptions, Chrome
+from selenium.webdriver.chrome.options import Options
+from guara.setup import OpenApp, CloseApp
+from guara.it import IsEqualTo, IsNotEqualTo
+from typing import List
+from examples.web_ui.selenium.simple.local_page.home import SubmitText
 
 
 class TestLocalPage:
-    def setup_method(self, method):
-        file_path = Path(__file__).parent.parent.parent.parent.resolve()
-        options = webdriver.ChromeOptions()
+    def setup_method(self) -> None:
+        file_path: Path = Path(__file__).parent.parent.parent.parent.resolve()
+        options: Options = ChromeOptions()
         options.add_argument("--headless=new")
-        self._app = Application(webdriver.Chrome(options=options))
+        self._app: Application = Application(Chrome(options=options))
         self._app.at(
-            setup.OpenApp,
+            transaction=OpenApp,
             url=f"file:///{file_path}/sample.html",
             window_width=1094,
             window_height=765,
-            implicitly_wait=0.5,
-        ).asserts(it.IsEqualTo, "Sample page")
+            delay=0.5,
+        ).asserts(IsEqualTo, "Sample page")
 
-    def teardown_method(self, method):
-        self._app.at(setup.CloseApp)
+    def teardown_method(self) -> None:
+        self._app.at(CloseApp)
 
-    def test_local_page(self):
-        text = ["cheese", "selenium", "test", "bla", "foo"]
-        text = text[randrange(len(text))]
-        self._app.at(home.SubmitText, text=text).asserts(
-            it.IsEqualTo, f"It works! {text}!"
+    def test_local_page(self) -> None:
+        items: List[str] = ["cheese", "selenium", "test", "bla", "foo"]
+        text: str = items[randrange(len(items))]
+        self._app.at(
+            transaction=SubmitText,
+            text=text
+        ).asserts(
+            assertion=IsEqualTo,
+            expected=f"It works! {text}!"
         )
-        self._app.at(home.SubmitText, text=text).asserts(it.IsNotEqualTo, "Any")
+        self._app.at(
+            transaction=SubmitText,
+            text=text
+        ).asserts(IsNotEqualTo, "Any")
