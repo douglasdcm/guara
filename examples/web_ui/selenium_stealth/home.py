e0ff4fab92afe89e79b633b1b3044d399ccf054f
theijhay <olawaleisaacjohn@gmail.com>
2025-01-30 21:35:06 +0100

    Refactor HomeTransactions do method to accept a text parameter

--- examples/web_ui/selenium_stealth/home.py
+++ examples/web_ui/selenium_stealth/home.py
@@ -4,7 +4,7 @@ from guara.transaction import AbstractTransaction
 class HomeTransactions(AbstractTransaction):
     """Actions on the home page"""
 
-    def do(self):
+    def do(self, text):
         self._driver.get("https://example.com")
         self._driver.find_element("id", "language-switcher").click()
         return self._driver.page_source
