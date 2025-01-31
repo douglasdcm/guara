45a34f0fbdc365aff695b143dc6e3628acc4090b
theijhay <olawaleisaacjohn@gmail.com>
2025-01-31 21:26:21 +0100

    Refactor HomeTransactions do method to update search box element locator and submit a text parameter

--- examples/web_ui/selenium_stealth/home.py
+++ examples/web_ui/selenium_stealth/home.py
@@ -6,5 +6,9 @@ class SubmitSeleniumStealth(AbstractTransaction):
 
     def do(self, text):
         self._driver.get("https://example.com")
+        TEXT = '//*[@id="input"]'
         self._driver.find_element("tag name", "h1").click()
+        text_field = self._driver.find_element_by_xpath(TEXT)
+        text_field.send_keys(text)
+        text_field.submit()
         return self._driver.page_source
