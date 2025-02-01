ed5e5a3747f0853349143045cba50af3d1b9a91a
DONALDBZR <andygaspard@hotmail.com>
2025-02-01 12:52:18 +0400

    [QA][0.0.9][0.0.33] The testing module that allows the testing of Selenium on local pages.

--- examples/web_ui/selenium/simple/local_page/test_local_page.py
+++ examples/web_ui/selenium/simple/local_page/test_local_page.py
@@ -2,6 +2,7 @@
 The testing module that allows the testing of Selenium on
 local pages.
 """
+
 from pathlib import Path
 from random import randrange
 from guara.transaction import Application
@@ -10,15 +11,21 @@ from selenium.webdriver.chrome.options import Options
 from guara.setup import OpenApp, CloseApp
 from guara.it import IsEqualTo, IsNotEqualTo
 from typing import List
-from examples.web_ui.selenium.simple.local_page.home import SubmitText
+from examples.web_ui.selenium.simple.local_page.home import (
+    SubmitText,
+)
 
 
 class TestLocalPage:
     def setup_method(self) -> None:
-        file_path: Path = Path(__file__).parent.parent.parent.parent.resolve()
+        file_path: Path = Path(
+            __file__
+        ).parent.parent.parent.parent.resolve()
         options: Options = ChromeOptions()
         options.add_argument("--headless=new")
-        self._app: Application = Application(Chrome(options=options))
+        self._app: Application = Application(
+            Chrome(options=options)
+        )
         self._app.at(
             transaction=OpenApp,
             url=f"file:///{file_path}/sample.html",
@@ -31,16 +38,20 @@ class TestLocalPage:
         self._app.at(CloseApp)
 
     def test_local_page(self) -> None:
-        items: List[str] = ["cheese", "selenium", "test", "bla", "foo"]
+        items: List[str] = [
+            "cheese",
+            "selenium",
+            "test",
+            "bla",
+            "foo",
+        ]
         text: str = items[randrange(len(items))]
         self._app.at(
-            transaction=SubmitText,
-            text=text
+            transaction=SubmitText, text=text
         ).asserts(
             assertion=IsEqualTo,
-            expected=f"It works! {text}!"
+            expected=f"It works! {text}!",
         )
         self._app.at(
-            transaction=SubmitText,
-            text=text
+            transaction=SubmitText, text=text
         ).asserts(IsNotEqualTo, "Any")
