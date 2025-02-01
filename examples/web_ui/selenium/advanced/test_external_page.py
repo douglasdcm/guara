c5dd38e3b627e999969a55129662621491839be1
DONALDBZR <andygaspard@hotmail.com>
2025-02-01 12:45:29 +0400

    [QA][0.0.9][0.0.32] The testing module that allows the testing of Selenium on external pages.

--- examples/web_ui/selenium/advanced/test_external_page.py
+++ examples/web_ui/selenium/advanced/test_external_page.py
@@ -2,8 +2,13 @@
 The testing module that allows the testing of Selenium on
 external pages.
 """
+
 from pytest import fixture
-from examples.web_ui.selenium.advanced import home, contact, info
+from examples.web_ui.selenium.advanced import (
+    home,
+    contact,
+    info,
+)
 from guara.transaction import Application
 from guara.setup import OpenApp, CloseApp
 from selenium.webdriver import ChromeOptions, Chrome
@@ -16,7 +21,9 @@ class TestVpmTransaction:
     def setup_method(self) -> None:
         options: Options = ChromeOptions()
         options.add_argument("--headless=new")
-        self._app: Application = Application(Chrome(options=options))
+        self._app: Application = Application(
+            Chrome(options=options)
+        )
         self._app.at(
             transaction=OpenApp,
             url="https://vagaspramim.onrender.com/",
@@ -33,9 +40,11 @@ class TestVpmTransaction:
         content_in_portuguese: str = "Conteúdo do currículo"
         self._app.at(home.ChangeToPortuguese).asserts(
             assertion=IsEqualTo,
-            expected=content_in_portuguese
+            expected=content_in_portuguese,
         )
-        result: Any = self._app.at(home.ChangeToEnglish).result
+        result: Any = self._app.at(
+            home.ChangeToEnglish
+        ).result
         IsEqualTo().asserts(result, content_in_english)
         self._app.at(info.NavigateTo).asserts(
             assertion=Contains,
@@ -45,45 +54,43 @@ class TestVpmTransaction:
             ),
         )
         self._app.at(home.NavigateTo).asserts(
-            assertion=IsEqualTo,
-            expected=content_in_english
+            assertion=IsEqualTo, expected=content_in_english
         )
         self._app.at(contact.NavigateTo).asserts(
             assertion=IsEqualTo,
-            expected="Contact us. We would be happy to answer your questions."
+            expected="Contact us. We would be happy to answer your questions.",
         )
         self._app.at(home.NavigateTo).asserts(
-            assertion=IsEqualTo,
-            expected=content_in_english
+            assertion=IsEqualTo, expected=content_in_english
         )
         self._app.at(
             transaction=home.DoRestrictedSearch,
             text=text,
-            wait_for=restricted_similariy
+            wait_for=restricted_similariy,
         ).asserts(
             assertion=IsEqualTo,
-            expected=restricted_similariy
+            expected=restricted_similariy,
         )
         self._app.at(home.NavigateTo).asserts(
-            assertion=IsEqualTo,
-            expected=content_in_english
+            assertion=IsEqualTo, expected=content_in_english
         )
         self._app.at(
             transaction=home.DoExpandedSearch,
             text=text,
-            wait_for=expanded_similarity
+            wait_for=expanded_similarity,
         ).asserts(
             assertion=IsEqualTo,
-            expected=expanded_similarity
+            expected=expanded_similarity,
         )
         self._app.at(home.NavigateTo).asserts(
-            assertion=IsEqualTo,
-            expected=content_in_english
+            assertion=IsEqualTo, expected=content_in_english
         )
 
 
 @fixture
-def setup_application() -> Generator[Application, Any, None]:
+def setup_application() -> (
+    Generator[Application, Any, None]
+):
     configuration: Dict[str, Union[str, int, float]] = {
         "url": "https://vagaspramim.onrender.com/",
         "window_width": 1094,
@@ -97,11 +104,13 @@ def setup_application() -> Generator[Application, Any, None]:
     yield app
     app.at(
         transaction=CloseApp,
-        screenshot_filename="./captures/guara-my-picture"
+        screenshot_filename="./captures/guara-my-picture",
     )
 
 
-def test_vpm_transaction_builder(setup_application: Application) -> None:
+def test_vpm_transaction_builder(
+    setup_application: Application,
+) -> None:
     app: Application = setup_application
     text: str = "software testing"
     restricted_similariy: str = "Similarity 10.7%"
@@ -109,44 +118,48 @@ def test_vpm_transaction_builder(setup_application: Application) -> None:
     content_in_english: str = "Content of curriculum"
     content_in_portuguese: str = "Conteúdo do currículo"
     app.at(home.ChangeToPortuguese).asserts(
-        assertion=IsEqualTo,
-        expected=content_in_portuguese
+        assertion=IsEqualTo, expected=content_in_portuguese
     ).at(home.ChangeToEnglish).asserts(
-        assertion=IsEqualTo,
-        expected=content_in_english
-    ).at(info.NavigateTo).asserts(
+        assertion=IsEqualTo, expected=content_in_english
+    ).at(
+        info.NavigateTo
+    ).asserts(
         assertion=Contains,
         expected=(
             "This project was born from the will of its collaborators to"
             " help people to find jobs more easily."
         ),
-    ).at(home.NavigateTo).asserts(
+    ).at(
+        home.NavigateTo
+    ).asserts(
+        assertion=IsEqualTo, expected=content_in_english
+    ).at(
+        contact.NavigateTo
+    ).asserts(
         assertion=IsEqualTo,
-        expected=content_in_english
-    ).at(contact.NavigateTo).asserts(
-        assertion=IsEqualTo,
-        expected="Contact us. We would be happy to answer your questions."
-    ).at(home.NavigateTo).asserts(
-        assertion=IsEqualTo,
-        expected=content_in_english
+        expected="Contact us. We would be happy to answer your questions.",
+    ).at(
+        home.NavigateTo
+    ).asserts(
+        assertion=IsEqualTo, expected=content_in_english
     ).at(
         transaction=home.DoRestrictedSearch,
         text=text,
-        wait_for=restricted_similariy
+        wait_for=restricted_similariy,
     ).asserts(
-        assertion=IsEqualTo,
-        expected=restricted_similariy
-    ).at(home.NavigateTo).asserts(
-        assertion=IsEqualTo,
-        expected=content_in_english
+        assertion=IsEqualTo, expected=restricted_similariy
+    ).at(
+        home.NavigateTo
+    ).asserts(
+        assertion=IsEqualTo, expected=content_in_english
     ).at(
         transaction=home.DoExpandedSearch,
         text=text,
-        wait_for=expanded_similarity
+        wait_for=expanded_similarity,
     ).asserts(
-        assertion=IsEqualTo,
-        expected=expanded_similarity
-    ).at(home.NavigateTo).asserts(
-        assertion=IsEqualTo,
-        expected=content_in_english
+        assertion=IsEqualTo, expected=expanded_similarity
+    ).at(
+        home.NavigateTo
+    ).asserts(
+        assertion=IsEqualTo, expected=content_in_english
     )
