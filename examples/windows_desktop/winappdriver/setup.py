2fcdf4bcfe71b427cea12e4b1ff642f1af90081b
theijhay <olawaleisaacjohn@gmail.com>
2025-02-02 00:45:46 +0100

    Refactor CalculatorTransactions to improve code readability and maintainability

--- examples/windows_desktop/winappdriver/setup.py
+++ examples/windows_desktop/winappdriver/setup.py
@@ -10,15 +10,15 @@ class OpenAppTransaction(AbstractTransaction):
             "app": "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App",
             "platformName": "Windows",
             "deviceName": "WindowsPC"
-        }  
+            }  
         self._driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723',
             desired_capabilities=desired_caps
-        )  
+            )  
         return self._driver
 
-class CloseAppTransaction(AbstractTransaction):  
-    """Close Calculator"""  
-    def do(self):  
-        if self._driver:  
+class CloseAppTransaction(AbstractTransaction):
+    """Close Calculator"""
+    def do(self):
+        if self._driver:
             self._driver.quit()
--- examples/windows_desktop/winappdriver/test_calculator.py
+++ examples/windows_desktop/winappdriver/test_calculator.py
@@ -7,12 +7,12 @@ class TestCalculator:
     """Test Windows Calculator using Guará + WinAppDriver"""
 
     def setup_method(self):
-        self.app = Application()  
+        self.app = Application()
         self.app.at(setup.OpenAppTransaction())
 
-    def teardown_method(self):  
+    def teardown_method(self):
         self.app.at(setup.CloseAppTransaction())
 
-    def test_addition(self):  
-        result = self.app.at(calculator.CalculatorTransactions(), num1=5, num2=3)  
+    def test_addition(self):
+        result = self.app.at(calculator.CalculatorTransactions(), num1=5, num2=3)
         result.asserts(calculator.CalculatorTransactions().validate_result, 8)
