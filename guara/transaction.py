99a92c175342dc069530c78d211cbea4e02eeb7d
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-02-07 01:19:29 -0300

    Add method 'when' and bump version

--- guara/transaction.py
+++ guara/transaction.py
@@ -72,6 +72,21 @@ class Application:
         self._result = self._transaction.do(**kwargs)
         return self
 
+    def when(self, transaction: AbstractTransaction, **kwargs: Dict[str, Any]) -> "Application":
+        """
+        Same as the `at` method. Introduced for better readability.
+
+        Performing a transaction.
+
+        Args:
+            transaction: (AbstractTransaction): The web transaction handler.
+            kwargs: (dict): It contains all the necessary data and parameters for the transaction.
+
+        Returns:
+            (Application)
+        """
+        return self.at(transaction, **kwargs)
+
     def asserts(self, assertion: IAssertion, expected: Any) -> "Application":
         """
         Asserting and validating the data by implementing the
