eaf5e57d891b2164d9712e3108ad178cb9fd13a9
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-03-15 22:48:40 -0300

    fix example

--- tests/test_guara_iDONi.py
+++ tests/test_guara_iDONi.py
@@ -1,8 +1,20 @@
-from guara import Application
-from guara.abstract_transaction import AbstractTransaction
+from guara.transaction import Application, AbstractTransaction
+from guara import it
+from guara.setup import OpenApp, CloseApp
+from selenium import webdriver
 
 app = Application(driver="selenium")
 
-app.add(AbstractTransaction())
-app.run()
-# it doesn't work, sadly
+
+class Demo(AbstractTransaction):
+    def do(self, **kwargs):
+        return self._driver.current_url
+
+
+def test_demo_idons():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    app = Application(webdriver.Chrome(options=options))
+    app.when(OpenApp, url="https://example.com/").asserts(it.IsEqualTo, "Example Domain")
+    app.when(Demo).asserts(it.IsEqualTo, "https://example.com/")
+    app.at(CloseApp)
