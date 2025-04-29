001a1c746017eb15b291245fd7cda4f0ab14a164
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-04-29 02:10:31 -0300

    Run caqui in background

--- examples/web_ui/caqui/asynchronouos/test_async.py
+++ examples/web_ui/caqui/asynchronouos/test_async.py
@@ -6,58 +6,68 @@
 from pathlib import Path
 from pytest_asyncio import fixture
 from pytest import mark
-from typing import Any, Dict, Union, Generator
+from typing import Any, Generator
 from guara.asynchronous.it import IsEqualTo
 from guara.asynchronous.transaction import Application
 from examples.web_ui.caqui.asynchronouos.home import GetNthLink
 from examples.web_ui.caqui.asynchronouos.setup import OpenApp, CloseApp
+from caqui.easy.options import ChromeOptionsBuilder
+from caqui.easy.capabilities import ChromeCapabilitiesBuilder
+from caqui.synchronous import get_session
+from caqui.easy.server import Server
+from time import sleep
+
+SERVER_PORT = 9999
+SERVER_URL = f"http://localhost:{SERVER_PORT}"
+file_path: Path = Path(__file__).parent.parent.parent.resolve()
+PAGE_URL = f"file:///{file_path}/sample.html"
 
 
-@mark.skip(
-    reason="before execute it start the driver as a service"
-    "https://github.com/douglasdcm/caqui/tree/main?tab=readme-ov-file#simple-start"
-)
-class TestAsyncTransaction:
+@fixture(scope="function")
+def setup_server():
+    server = Server.get_instance(port=SERVER_PORT)
+    server.start()
+    yield
+    sleep(3)
+    server.dispose()
+
+
+@fixture
+def setup_environment(setup_server):
+    options = ChromeOptionsBuilder().args(["headless"]).to_dict()
+    capabilities = (
+        ChromeCapabilitiesBuilder().accept_insecure_certs(True).add_options(options)
+    ).to_dict()
+    yield capabilities
+
+
+class TestAsyncTransactionCaqui:
     """
     The test class for asynchronuous transaction.
     """
 
     @fixture(loop_scope="function")
-    async def setup_test(self) -> Generator[None, Any, None]:  # type: ignore
+    async def setup_test(self, setup_environment) -> Generator[None, Any, None]:  # type: ignore
         """
         Setting up the transaction for the test.
 
         Returns:
             (Generator[None, Any, None])
         """
-        # Lazy import to avoid installation of the library
-        from caqui.easy.capabilities import CapabilitiesBuilder
-        from caqui.synchronous import get_session
-
-        file_path: Path = Path(__file__).parent.parent.parent.parent.resolve()
-        self._driver_url: str = "http://127.0.0.1:9999"
-        capabilities: Dict[str, Dict[Union[Any, str], Any]] = (
-            CapabilitiesBuilder()
-            .browser_name("chrome")
-            .accept_insecure_certs(True)
-            .additional_capability(
-                {"goog:chromeOptions": {"extensions": [], "args": ["--headless"]}}
-            )
-        ).build()
-
-        self._session: Any = get_session(self._driver_url, capabilities)
+        capabilities = setup_environment
+        self._session: Any = get_session(SERVER_URL, capabilities)
         self._app: Application = Application(self._session)
         await self._app.at(
             transaction=OpenApp,
             with_session=self._session,
-            connect_to_driver=self._driver_url,
+            connect_to_driver=SERVER_URL,
             access_url=f"file:///{file_path}/sample.html",
         ).perform()
         yield
         await self._app.at(
             transaction=CloseApp,
             with_session=self._session,
-            connect_to_driver=self._driver_url,
+            connect_to_driver=SERVER_URL,
         ).perform()
 
     @mark.asyncio
@@ -69,5 +79,5 @@ class TestAsyncTransaction:
             transaction=GetNthLink,
             link_index=1,
             with_session=self._session,
-            connect_to_driver=self._driver_url,
+            connect_to_driver=SERVER_URL,
         ).asserts(IsEqualTo, "any1.com").perform()
