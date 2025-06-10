980f4d85b9e1a1cc759d718bedfe645313970f9d
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-06-10 17:13:21 -0300

    Bump version (#141)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Douglas Cardoso"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.0.13"
+release = "0.0.14"
 
 
 # -- General configuration ---------------------------------------------------
