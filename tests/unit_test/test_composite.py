dc2806bc8090bc33ced63ef3d354b584712a636d
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2026-04-14 16:04:53 -0300

    fix linter issues

--- tests/unit_test/test_composite.py
+++ tests/unit_test/test_composite.py
@@ -7,9 +7,11 @@ class DoThat(AbstractTransaction):
     def do(self, **kwargs):
         return f"do that {kwargs.get('param')}"
 
+
 class DoIt(AbstractTransaction):
     def do(self, **kwargs):
         return f"do it {kwargs.get('param')}"
+
 
 class DoAll(AbstractTransaction):
     def do(self, **kwargs):
@@ -19,6 +21,7 @@ class DoAll(AbstractTransaction):
             result = to_do().do(**kwargs)
             results.append(result)
         return results
+
 
 def test_composite_class():
     Application().when(DoAll, param="foo").then(it.ContainsAll, ["do that foo", "do it foo"])
